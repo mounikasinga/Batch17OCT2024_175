713713d20eb75f387f3e6640ee022c57fad35d15
bsuyash2711 <singamounika2003@gmail.com>
2024-11-09 11:32:46 +0000

    initial commit

---
+++ Assignments/loop_changetoexitcontrol.c
@@ -0,0 +1,13 @@
+ /*loop is change entry control to exit control loop*/
+#include<stdio.h>
+ int main(){
+ for(int i=1;;i++){
+         printf("%d",i);
+         if(i>=5)
+         {
+                break;
+        }
+}
+
+}
+
