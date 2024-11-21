5ad2f3b9642bc70bb51cc02e879b683cc3584bd0
bsuyash2711 <singamounika2003@gmail.com>
2024-11-21 06:23:36 +0000

    delete

---
+++ ClassWork/day18/search.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+int binarySearch(int arr[], int key) {
+    int low=1;,high=1;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] == key) {
+            return mid; 
+        }
+        if (arr[mid] < key) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    return -1; 
+}
+
+int main() {
+    int *a, n, key, i;
+    printf("Enter n value: ");
+    scanf("%d", &n);
+    printf("Enter the key to search: ");
+    scanf("%d", &key);
+    a = (int *)malloc(n * sizeof(int)); 
+    printf("Enter %d elements in sorted order:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]); 
+    int result = binarySearch(a, key);
+    
+    if (result != -1) {
+        printf("Key is found at index %d\n", result);
+    } else {
+        printf("Key is not found\n");
+    }
+    free(a); 
+    return 0;
+}
+
---
+++ ClassWork/day18/search_rec.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int binarySearchRecursive(int arr[], int low, int high, int key) {
+    if (low > high) {
+        return -1;
+    }
+    int mid = low + (high - low) / 2;
+    if (arr[mid] == key) {
+        return mid; 
+    }
+    else if (arr[mid] < key) {
+        return binarySearchRecursive(arr, mid + 1, high, key); 
+    }
+    else
+        return binarySearchRecursive(arr, low, mid - 1, key);
+}
+
+int main() {
+    int *a, n, key, i;
+    printf("Enter n value: ");
+    scanf("%d", &n);
+    printf("Enter the key: ");
+    scanf("%d", &key);
+    a = (int *)malloc(n * sizeof(int));
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+    int result = binarySearchRecursive(a, 0, n - 1, key);
+    if (result != -1) {
+        printf("\nKey is found\n");
+    } else {
+        printf("\nKey is not found\n");
+    }
+    free(a);
+    printf("\n\n");
+    return 0;
+}
+
